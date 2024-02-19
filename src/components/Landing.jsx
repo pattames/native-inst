cc0dfd8184a25e3c2b9e562cd9c792b0a4c7aaa5
Tamés <patriciotames@patricios-mini.speedport.ip>
2024-02-19 12:47:28 +0100

    navbar

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,8 +1,13 @@
+import Navbar from "./Navbar";
+import styles from "./modules/Landing.module.css";
+
 //Patricio
 export default function Landing() {
   return (
     <>
       <h2>Landing</h2>
+      <Navbar />
+      <div className={styles.hero}></div>
     </>
   );
 }
